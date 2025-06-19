7c0dcee85d8ca5e17b5ef5765ab73d83666a231e
Benny Degezelle <benny@morse.be>
2025-06-19 21:33:21 +0200

    Rephrase 'your member areas' to 'dashboard' in r:hello_or_login

--- lib/marketplace.rb
+++ lib/marketplace.rb
@@ -2,7 +2,7 @@ module Marketplace
   include Radiant::Taggable
   desc 'Tags to load marketplace into the current page'
 
-  nav_links = "<a href='/account/'>Your Members Areas</a> | 
+  nav_links = "<a href='/account/'>Dashboard</a> | 
                <a href='/membership/details/'>Your Account</a> |
                <a href='/account/logout'>Logout</a>"
   tag 'hello_or_login' do |tag|
